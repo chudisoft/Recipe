45094491997e9c7eba9252a0c437f843b262a9e8
MayPyone <maypyone015@gmail.com>
2024-01-29 22:15:06 +0630

    Update food controller

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,7 +1,46 @@
 class FoodsController < ApplicationController
-  def index; end
-  def show; end
-  def new; end
-  def create; end
-  def destroy; end
+  before_action :set_food, only: %i[destroy]
+
+  # GET /foods or /foods.json
+  def index
+    @foods = Food.where(user_id: current_user.id)
+  end
+
+  # GET /foods/new
+  def new
+    @food = Food.new
+  end
+
+  def create
+    @food = Food.new(food_params)
+    @food.user = current_user
+
+    if @food.save
+      redirect_to foods_path
+    else
+      render 'new'
+    end
+  end
+
+  # DELETE /foods/1 or /foods/1.json
+  def destroy
+    @food = Food.find(params[:id])
+    if @food.destroy
+      redirect_to foods_path
+    else
+      flash[:alert] = 'food could not be deleted.'
+    end
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_food
+    @food = Food.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity, :user_id)
+  end
 end
