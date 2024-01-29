5bb4ad9ffccff1239f29cef2174578ef3fdfb5be
MayPyone <maypyone015@gmail.com>
2024-01-29 18:40:25 +0630

    Add food routes

---
+++ app/controllers/recipe_foods_controller.rb
@@ -0,0 +1,6 @@
+class RecipesFoodsController < ApplicationController
+    def new; end
+    def create; end
+    def destroy; end
+    def general_shopping_list; end
+end
---
+++ config/routes.rb
@@ -0,0 +1,21 @@
+Rails.application.routes.draw do
+  devise_for :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  resources :foods, except: [:update, :edit]
+
+  resources :recipes, except: [:edit] do
+    resources :recipe_foods, only: [:new, :create, :destroy]
+  end
+
+  get '/public_recipes', to: 'recipes#public_recipes'
+
+  get '/general_shopping_list', to: "recipe_foods#general_shopping_list"
+
+  # Defines the root path route ("/")
+  root "foods#index"
+end
